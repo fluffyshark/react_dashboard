f6af4908045b748efccd6acdbe5576f2a0460bbe
Robin <robin.yjord1@gmail.com>
2020-04-06 07:40:25 +0200

    Updates

---
+++ precache-manifest.bb9b2ad92a45a4f88b4f149d5fdb7a21.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdbf8f76e50807d9aa41fe6d5ecb7cf0",
+    "url": "/react_dashboard/index.html"
+  },
+  {
+    "revision": "ae59fb0e96894f68c516",
+    "url": "/react_dashboard/static/css/main.a33efb04.chunk.css"
+  },
+  {
+    "revision": "0f81450c2f3e070efc93",
+    "url": "/react_dashboard/static/js/2.09f1f0bd.chunk.js"
+  },
+  {
+    "revision": "d2cf8d37946cd02abf2fe73b5ccb09c4",
+    "url": "/react_dashboard/static/js/2.09f1f0bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae59fb0e96894f68c516",
+    "url": "/react_dashboard/static/js/main.5a3000ad.chunk.js"
+  },
+  {
+    "revision": "c4dc09c72deba857c998",
+    "url": "/react_dashboard/static/js/runtime-main.daa14961.js"
+  },
+  {
+    "revision": "8b9aa269ae4b582660554071b62d16e9",
+    "url": "/react_dashboard/static/media/arrow-icon.8b9aa269.svg"
+  },
+  {
+    "revision": "e71c0b78357cb8d305ab6d547fab760a",
+    "url": "/react_dashboard/static/media/building-1.e71c0b78.png"
+  },
+  {
+    "revision": "2aeec0699c3eecc2bf0aa4332bd42ab3",
+    "url": "/react_dashboard/static/media/building-2.2aeec069.png"
+  },
+  {
+    "revision": "a8ac6203675f6554a4e6081936c9693f",
+    "url": "/react_dashboard/static/media/building-3.a8ac6203.png"
+  },
+  {
+    "revision": "7502b96cfd06dca0cb879d549aa43913",
+    "url": "/react_dashboard/static/media/building-4.7502b96c.png"
+  },
+  {
+    "revision": "62259b1a57bdf9bdd202b04d9ed5422b",
+    "url": "/react_dashboard/static/media/city_image.62259b1a.png"
+  },
+  {
+    "revision": "2777891df09f44cc2cb38c32edf204ed",
+    "url": "/react_dashboard/static/media/green_arrow.2777891d.png"
+  },
+  {
+    "revision": "f10b1ace4dc9375b37b8aa4e4b34236b",
+    "url": "/react_dashboard/static/media/profile_img.f10b1ace.png"
+  },
+  {
+    "revision": "a9028a6b9a33f45add34c30e0b200ea7",
+    "url": "/react_dashboard/static/media/search-icon.a9028a6b.svg"
+  },
+  {
+    "revision": "a2d4d1acf563eefdf3e12b4f144765b9",
+    "url": "/react_dashboard/static/media/side_building.a2d4d1ac.png"
+  },
+  {
+    "revision": "118979d4eb0cf747677c76b07e0d11ee",
+    "url": "/react_dashboard/static/media/turnover_icon.118979d4.png"
+  }
+]);
